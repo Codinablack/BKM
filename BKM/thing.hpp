55b08c96e0a01cb6810ee1780cb5a324bab6be25
codinablack <codinablack@gmail.com>
2023-12-09 18:04:18 -0600

    Started the makings of "Thing" and "Creature" classes to be used as base classes.

--- BKM/thing.hpp
+++ BKM/thing.hpp
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef _THING_HPP
+#define _THING_HPP
+
+class Thing
+{
+
+
+public:
+	constexpr Thing() = default;
+	virtual ~Thing() = default;
+
+	// non-copyable
+	Thing(const Thing&) = delete;
+	Thing& operator=(const Thing&) = delete;
+
+};
+
+
+#endif
