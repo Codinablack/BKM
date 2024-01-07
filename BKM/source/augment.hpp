21b313ceb04409d9f050068e4c274d678674173e
codinablack <codinablack@gmail.com>
2024-01-06 20:13:20 -0600

    Basic starter layout for augments

--- BKM/source/augment.hpp
+++ BKM/source/augment.hpp
@@ -1,10 +1,35 @@
 #ifndef _AUGMENT_HPP
 #define _AUGMENT_HPP
 
+#include <vector>
 
 #include "console.hpp"
 
 namespace BKM {
+	/// Forward Declarations
+	class PointBasedStat;
+	class LevelBasedSkill;
+
+
+
+	
+	class Augment {
+		/// 0 id, 1 ModType, 2 ModValue
+		using Modifier = std::array<int64_t, 3>;
+
+		Augment(std::vector<Modifier> mods){
+			_modifiers = mods;
+		}
+
+
+		Augment operator + (Augment& ) {/*maybe make it possible to add together augments, but probably better for modifiers*/
+
+		}
+
+	private:
+		std::vector<Modifier> _modifiers;
+	};
+
 	/// make equippable to items, players, and mounts.
 
 } // namespace BKM
